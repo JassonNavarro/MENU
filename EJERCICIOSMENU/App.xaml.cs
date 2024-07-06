117944678be5656fbf0d9182bcb685435e1fc6ab
50237 <50237@DESKTOP-AC292QU>
2024-07-06 06:14:48 +0200

    Agregar archivos de proyecto.

---
+++ EJERCICIOSMENU/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace EJERCICIOSMENU
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
